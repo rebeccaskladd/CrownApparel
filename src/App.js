88330df80457ef77d41830b5900b46622f0787ed
Rebecca Skladd <rebecca.skladd@gmail.com>
2022-04-23 15:58:44 -0400

    Navigation bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
 
+import Navigation from './routes/Navigation/Navigation';
 import Home from './routes/Home/Home';
+
+const Shop = () => {
+    return (
+        <div>
+            <h1>I am the shop page</h1>
+        </div>
+    )
+}
 
 const App = () => {
 
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Navigation />}>
+                <Route index element={<Home />} />
+                <Route path="/shop" element={<Shop />} />
+            </Route>
         </Routes>
     );
 }
---
+++ src/routes/Navigation/Navigation.jsx
@@ -0,0 +1,35 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from "react-router-dom";
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
+import './Navigation.scss';
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className='navigation'>
+                <Link className='logo-container' to="/">
+                    <CrwnLogo className='logo' />
+                </Link>
+                <div className="nav-links">
+                    <Link className='nav-link' to="/shop">
+                        Shop
+                    </Link>
+                    <Link className='nav-link' to="/contact">
+                        Contact
+                    </Link>
+                    <Link className='nav-link' to="/signin">
+                        Sign In
+                    </Link>
+                    <Link className='nav-link' to="/viewcart">
+                        View Cart
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    )
+}
+
+export default Navigation;
