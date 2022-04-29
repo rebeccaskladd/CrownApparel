a31b808446cecdce97bbfa7df713f04144cea353
Rebecca Skladd <rebecca.skladd@gmail.com>
2022-04-29 12:03:32 -0400

    Sign In nav

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Navigation from './routes/Navigation/Navigation';
 import Home from './routes/Home/Home';
+import SignIn from './routes/SignIn/SignIn';
 
 const Shop = () => {
     return (
@@ -18,6 +19,7 @@ const App = () => {
             <Route path="/" element={<Navigation />}>
                 <Route index element={<Home />} />
                 <Route path="/shop" element={<Shop />} />
+                <Route path="/signin" element={<SignIn />} />
             </Route>
         </Routes>
     );
