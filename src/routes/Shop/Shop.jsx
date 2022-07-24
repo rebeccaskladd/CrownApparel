7a2d0ae25e129cbdef4ad7df7733b6f9d88558fd
Rebecca Skladd <rebecca.skladd@gmail.com>
2022-07-24 18:35:31 -0400

    add redux thunk

--- src/routes/Shop/Shop.jsx
+++ src/routes/Shop/Shop.jsx
@@ -4,19 +4,13 @@ import { useDispatch } from 'react-redux';
 
 import CategoriesPreview from '../CategoriesPreview/CategoriesPreview';
 import Category from '../Category/Category';
-import { getCategoriesAndDocuments } from '../../utilities/firebase/firebase';
-import { setCategories } from '../../store/categories/categories.action';
+import { fetchCategoriesAsync } from '../../store/categories/categories.action';
 
 const Shop = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        const getCategoriesMap = async () => {
-            const categoriesArray = await getCategoriesAndDocuments('categories');
-            dispatch(setCategories(categoriesArray));
-        }
-
-        getCategoriesMap();
+        dispatch(fetchCategoriesAsync());
     }, []);
 
     return (
--- src/store/categories/categories.action.js
+++ src/store/categories/categories.action.js
@@ -1,5 +1,26 @@
 import { CATEGORIES_ACTION_TYPES } from "./categories.types"
+import { getCategoriesAndDocuments } from "../../utilities/firebase/firebase";
 
-export const setCategories = (categoriesArray) => {
-    return { type: CATEGORIES_ACTION_TYPES.SET_CATEGORIES, payload: categoriesArray };
-}
+export const fetchCategoriesStart = () => {
+    return { type: CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START };
+};
+
+export const fetchCategoriesSuccess = (categoriesArray) => {
+    return { type: CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, payload: categoriesArray };
+};
+
+export const fetchCategoriesFailed = (error) => {
+    return { type: CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, payload: error };
+};
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+    dispatch(fetchCategoriesStart());
+
+    try {
+        const categoriesArray = await getCategoriesAndDocuments('categories');
+        dispatch(fetchCategoriesSuccess(categoriesArray));
+    }
+    catch (error) {
+        dispatch(fetchCategoriesFailed(error));
+    }
+};
