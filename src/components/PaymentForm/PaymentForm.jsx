b58846049d1949d99fa4c8e652973fc602ac3013
Rebecca Skladd <rebecca.skladd@gmail.com>
2022-07-27 17:19:21 -0400

    stripe payment setup

--- src/components/PaymentForm/PaymentForm.jsx
+++ src/components/PaymentForm/PaymentForm.jsx
@@ -14,11 +14,37 @@ const PaymentForm = () => {
         if (!stripe || !elements) {
             return;
         }
-    }
+
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ amount: 10000 })
+        }).then(res => res.json());
+
+        const { paymentIntent: { client_secret } } = response;
+
+        const paymentResult = await stripe.confirmCardPayment(client_secret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: 'Test Test'
+                }
+            }
+        });
+
+        if (paymentResult.error) {
+            alert(paymentResult.error)
+        }
+        else {
+            alert('Payment successful');
+        }
+    };
 
     return (
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer onSubmit={paymentHandler}>
                 <h2>Credit Card Payment</h2>
                 <CardElement />
                 <Button buttonType="inverted">Pay Now</Button>
