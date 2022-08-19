fb145cb3676b8534a3c48a40a91334debcdc15cf
Rebecca Skladd <rebecca.skladd@gmail.com>
2022-08-19 14:28:47 -0400

    mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { GlobalStyle } from './global.styles';
+
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
@@ -19,14 +21,17 @@ const App = () => {
     }, []);
 
     return (
-        <Routes>
-            <Route path="/" element={<Navigation />}>
-                <Route index element={<Home />} />
-                <Route path="/shop/*" element={<Shop />} />
-                <Route path="/auth" element={<Authentication />} />
-                <Route path="/checkout" element={<Checkout />} />
-            </Route>
-        </Routes>
+        <div>
+            <GlobalStyle />
+            <Routes>
+                <Route path="/" element={<Navigation />}>
+                    <Route index element={<Home />} />
+                    <Route path="/shop/*" element={<Shop />} />
+                    <Route path="/auth" element={<Authentication />} />
+                    <Route path="/checkout" element={<Checkout />} />
+                </Route>
+            </Routes>
+        </div>
     );
 }
 
